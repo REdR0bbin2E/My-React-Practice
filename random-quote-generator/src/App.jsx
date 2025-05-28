8b19db9e23be1c47d617671e4b2c3b7d4134fd22
REdR0bbin2E <mayo.akin2@gmail.com>
2025-05-28 17:35:57 -0500

    changes

--- random-quote-generator/src/App.jsx
+++ random-quote-generator/src/App.jsx
@@ -1,35 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import { useState, useEffect } from 'react';
+import './App.css';
+import { AnimatePresence, motion } from 'framer-motion';
 function App() {
-  const [count, setCount] = useState(0)
 
+
+
+
+  const [quotes, setQuotes] = useState([]);
+  const [index, setIndex] = useState(0);
+
+
+  useEffect(() => {
+    fetch("https://zenquotes.io/api/random") //ask the API
+      .then(res => res.json())  //this is me converting it to a useable JSON
+      .then(data => {
+        setQuotes(data) //Store the quotes in my state
+        setIndex(Math.floor(Math.random() * data.length)); //Pick a random quote
+      });
+  }, [])
+
+
+
+  const getRandomQuote = () => {
+    const randomIndex = Math.floor(Math.random() * quotes.length);
+    setIndex(randomIndex);
+  };
+
+  if (quotes.length === 0) {
+    return <p>Loading quotes...
+    </p>
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="App" style={{ padding: "2rem", textAlign: "center" }}>
+      <h1>Random Quote Generator</h1>
+
+      <motion.p whileTap={{ scale: 0.9 }} whileHover={{ scale: 1.1 }} initial={{ opacity: 0, color: "blue" }} animate={{ animationDelay: 5, opacity: 1, color: "blue", fontSize: 55 }} transition={{ duration: 5, repeat: 2 }} style={{ fontSize: "1.5rem", marginBottom: "1rem" }}>{quotes[index]?.a || "Unknown"}, {quotes[index]?.q}
+      </motion.p>
+
+      <button onClick={getRandomQuote}>Show Another Quote</button>
+    </div>
+  );
 }
 
-export default App
+export default App;
